e7c9329e000b267c7eca01e71350fd0ac839eec5
venegu <venegu@gmail.com>
2015-12-31 22:18:43 -0500

    Remembering bill amount across app restarts I think

--- tabby/ViewController.swift
+++ tabby/ViewController.swift
@@ -18,19 +18,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
-        
         // Initial currency formatting when app starts
         let currencySymbol = NSLocale.currentLocale().objectForKey(NSLocaleCurrencySymbol) as! String
         billField.placeholder = (currencySymbol as String) + "0.00"
         tipLabel.text = (currencySymbol as String) + "0.00"
         totalLabel.text =  (currencySymbol as String) + "0.00"
         
+        //Setting previous bill amount when the app closed after checking that billDate returns valid values and comparing the times
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if(defaults.objectForKey("billDate") != nil && NSDate.init().timeIntervalSinceDate(defaults.objectForKey("billDate") as! NSDate) < 600) {
+            let oldAmount = defaults.objectForKey("billAmount")
+            billField.text = (oldAmount as! String)
+        }
+        
         // Making the keyboard appear upon opening the app
         billField.becomeFirstResponder()
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -50,6 +55,13 @@ class ViewController: UIViewController {
         
         tipLabel.text = formatter.stringFromNumber(tip)
         totalLabel.text = formatter.stringFromNumber(total)
+        
+        //Storing bill amount
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setObject(billField.text, forKey:"billAmount")
+        defaults.setObject(NSDate.init(), forKey: "billDate")
+        defaults.synchronize()
     }
 
     @IBAction func onTap(sender: AnyObject) {
