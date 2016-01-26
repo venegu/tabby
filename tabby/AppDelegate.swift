58356eb3862325d775e3c0a615a73fb794911570
venegu <venegu@gmail.com>
2016-01-25 23:46:59 -0500

    Adding navigation bar color

--- tabby/AppDelegate.swift
+++ tabby/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.0/255.0, green: 122.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
         return true
     }
 
